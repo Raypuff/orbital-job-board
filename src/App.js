e48eebc4f0d8a0a052a010be8feb92ad46ec52a0
Raypuff <raynerljm@gmail.com>
2021-05-29 15:25:50 +0800

    Updated meta tags

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import LandingPage from "./pages/LandingPage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext"
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-      <Switch>
-        <Route path="/sign_in">
-          <SignInPage />
-        </Route>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path="/register">
-          <SignUpPage />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/sign_in">
+            <SignInPage />
+          </Route>
+          <Route path="/register">
+            <SignUpPage />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
       </Router>
     </AuthProvider>
   );
