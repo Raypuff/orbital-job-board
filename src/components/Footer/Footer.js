80dbd666906a6c11f0abf2ab2f6e918de1434e28
Raypuff <raynerljm@gmail.com>
2021-05-29 23:41:20 +0800

    Updated footer badly

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,46 @@
 import styles from "./Footer.module.css";
+import address from "../../assets/location-arrow-solid.svg";
+import phone from "../../assets/phone-alt-solid.svg";
+import email from "../../assets/envelope-solid.svg";
 
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.breadcrumb}>breadcrumbs</div>
-      <div className={styles.addressPanel}>
-        <h2 className={styles.addressHeader}>
-          National University of Singapore
-        </h2>
-        <p className={styles.addressLink}>
-          21 Lower Kent Ridge Road
-          <br />
-          Singapore 119077
-          <br />
-          +65 6516 6666
-          <br />
-          ccsgp@nus.edu.sg
-        </p>
+      <div className={styles.breadcrumbs}>
+        <div className={styles.container}>
+          <ul>
+            <li> </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles.copyrightBox}>
-        <p>National University of Singapore. All Rights Reserved.</p>
-        <div className={styles.copyrightLinks}>
-          <p>Legal</p>
-          <p className={styles.dot}>•</p>
-          <p>Branding guidelines</p>
+      <div className={styles.footer}>
+        <div className={styles.addressPanel}>
+          <div>
+            <h3 className={styles.addressHeader}>
+              National University of Singapore
+            </h3>
+            <ul className={styles.addressLink}>
+              <li>
+                21 Lower Kent Ridge Road
+                <br />
+                Singapore 119077
+              </li>
+              <li>+65 6516 6666</li>
+              <li>
+                <a href="mailto:ccsgp@nus.edu.sg">ccsgp@nus.edu.sg</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.copyrightBox}>
+          <div>
+            <p>National University of Singapore. All Rights Reserved.</p>
+            <div className={styles.copyrightLinks}>
+              <p>Legal</p>
+              <p className={styles.dot}>•</p>
+              <p>Branding guidelines</p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
