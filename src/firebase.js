46238f64d093b13365461b21732c0553325b2482
Zechary <zecharyajw@u.nus.edu>
2021-05-30 15:34:26 +0800

    Implementation of Firestore for Job Board
    Job Board now retrieves data from firestore and displays
    each job in a separate card on the Job Board. Functionality
    achieved using a getJobs() function that queries a snapshot
    of the data from the firestore database, and stores it into
    the jobs array. The jobs array is then rendered onto the Job Board
    page using a mapper function. The retrieval happens in real time,
    which means that any change made on the firestore would automatically
    update the information on the job board.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyCCnYK0KUEuGRwXGL4KZsc78Z0ACkIJTgM",
   authDomain: "nus-orbital-development.firebaseapp.com",
   projectId: "nus-orbital-development",
   storageBucket: "nus-orbital-development.appspot.com",
   messagingSenderId: "395419282206",
-  appId: "1:395419282206:web:5451d1ed6655ea4318370f"
-});
+  appId: "1:395419282206:web:5451d1ed6655ea4318370f",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
 
 export const auth = app.auth();
 export default app;
