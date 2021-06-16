d55552d1eeabb92424432f34bf6ce315cecc5f1e
Rayner <raynerljm@gmail.com>
2021-06-16 13:39:35 +0800

    Change buttonRow bottom margin to change with height

--- src/components/Landing/LandingOld.js
+++
@@ -1,9 +0,0 @@
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import styles from "./Landing.module.css";
-
-const Landing = () => {
-  return <div></div>;
-};
-
-export default Landing;
