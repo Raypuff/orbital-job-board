e83af748595bb1a1b1144260ba43a05be4201204
Rayner <raynerljm@gmail.com>
2021-06-21 19:37:50 +0800

    Added inline form validation for SignUpOrg

--- src/components/ORG/SignUpOrgForm/SignUpOrgForm.js
+++ src/components/ORG/SignUpOrgForm/SignUpOrgForm.js
@@ -3,138 +3,200 @@ import { Form, Button, Alert, Card } from "react-bootstrap";
 import styles from "./SignUpOrgForm.module.css";
 import { useAuth } from "../../../contexts/AuthContext";
 import { Link } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const SignUpOrgForm = () => {
-  //ref values to access form data
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
+	//ref values to access form data
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const passwordConfirmRef = useRef();
 
-  //imported methods from firebase authentication
-  const { signup, logout, sendEmailVerification } = useAuth();
+	//imported methods from firebase authentication
+	const { signup, logout, sendEmailVerification } = useAuth();
 
-  //initializing useStates for signup
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
+	//initializing useStates for signup
+	const [error, setError] = useState("");
+	const [message, setMessage] = useState("");
 
-  async function handleSubmit(event) {
-    //prevent page from refreshing
-    event.preventDefault();
+	const mySubmit = (values, { setSubmitting, resetForm }) => {
+		setSubmitting(true);
+		setTimeout(() => {
+			handleSubmit(values);
+			resetForm();
+			setSubmitting(false);
+		}, 500);
 
-    //resetting signup STATES
-    setMessage("");
-    setError("");
+		async function handleSubmit(values) {
+			//resetting signup STATES
+			setMessage("");
+			setError("");
 
-    //checking for errors in code
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
+			try {
+				//firebase side methods
+				await signup(values.email, values.password, "organization");
+				await sendEmailVerification();
+				await logout();
 
-    try {
-      //start of signup process
-      setLoading(true);
+				//set message to signify signup is successful
+				setMessage("Sign up successful!");
 
-      //firebase side methods
-      await signup(
-        emailRef.current.value,
-        passwordRef.current.value,
-        "organization"
-      );
-      await sendEmailVerification();
-      await logout();
+				//creating variables to send over through http request
+				const id = values.email;
 
-      //set message to signify signup is successful
-      setMessage("Sign up successful!");
+				//send account to backend
+				const body = { id };
+				await fetch(
+					"https://volunteer-ccsgp-backend.herokuapp.com/organization_accounts",
+					{
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify(body),
+					}
+				);
+			} catch (err) {
+				if (err.code === "auth/email-already-in-use") {
+					setError("This email address is already in use");
+				}
+			}
+		}
+	};
 
-      //creating variables to send over through http request
-      const id = emailRef.current.value;
+	return (
+		<div className={styles.formBG}>
+			<div className={styles.formContainer}>
+				<Card bg="light" text="dark" style={{ width: "23rem" }}>
+					<Card.Header as="h6">Sign up as an organization</Card.Header>
+					<Card.Body>
+						<Formik
+							initialValues={{ email: "", password: "", passwordConfirm: "" }}
+							validationSchema={validationSchema}
+							onSubmit={mySubmit}
+						>
+							{({
+								values,
+								touched,
+								errors,
+								handleChange,
+								handleBlur,
+								handleSubmit,
+								isSubmitting,
+							}) => (
+								<Form onSubmit={handleSubmit}>
+									<Form.Group controlId="formBasicEmail">
+										<Form.Label>Organization email address</Form.Label>
+										<Form.Control
+											name="email"
+											type="email"
+											value={values.email}
+											placeholder="Enter your email address"
+											onChange={handleChange}
+											onBlur={handleBlur}
+											isValid={touched.email && !errors.email}
+											isInvalid={touched.email && errors.email}
+										/>
+										<Form.Control.Feedback type="invalid">
+											{errors.email}
+										</Form.Control.Feedback>
+									</Form.Group>
 
-      //send account to backend
-      const body = { id };
-      await fetch(
-        "https://volunteer-ccsgp-backend.herokuapp.com/organization_accounts",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-        }
-      );
-    } catch (err) {
-      if (err.code === "auth/email-already-in-use") {
-        setError("Email already in use");
-      }
-      console.log(err.code);
-    }
-    setLoading(false);
-  }
-
-  return (
-    <div className={styles.formBG}>
-      <div className={styles.formContainer}>
-        <Card bg="light" text="dark" style={{ width: "23rem" }}>
-          <Card.Header as="h6">Sign up as an organization</Card.Header>
-          <Card.Body>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Organization email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  ref={emailRef}
-                  required
-                />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  ref={passwordRef}
-                  required
-                />
-                <Form.Text id="passwordReq" muted>
-                  Your password must be at least 6 characters long
-                </Form.Text>
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  ref={passwordConfirmRef}
-                  required
-                />
-              </Form.Group>
-              <Button disabled={loading} variant="primary" type="submit">
-                Sign up
-              </Button>
-            </Form>
-            <Card.Text />
-            {error && <Alert variant="danger">{error}</Alert>}
-            {message && (
-              <Alert variant="success">
-                <Alert.Heading as="h6">{message}</Alert.Heading>
-                <hr />
-                <p className="mb-0">
-                  Please check your inbox for a verification email
-                </p>
-              </Alert>
-            )}
-            <Card.Text />
-            <Card.Footer>
-              Already have an organization account?{" "}
-              <Link to="/sign-in-organization">Sign in here!</Link>
-            </Card.Footer>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
-  );
+									<Form.Group controlId="formBasicPassword">
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											name="password"
+											type="password"
+											value={values.password}
+											placeholder="Enter your password"
+											onChange={handleChange}
+											onBlur={handleBlur}
+											isValid={touched.password && !errors.password}
+											isInvalid={touched.password && errors.password}
+										/>
+										<Form.Control.Feedback type="invalid">
+											{errors.password}
+										</Form.Control.Feedback>
+									</Form.Group>
+									<Form.Group controlId="formBasicPassword">
+										<Form.Label>Confirm Password</Form.Label>
+										<Form.Control
+											name="passwordConfirm"
+											type="password"
+											value={values.passwordConfirm}
+											placeholder="Enter your password again"
+											onChange={handleChange}
+											onBlur={handleBlur}
+											isValid={
+												touched.passwordConfirm && !errors.passwordConfirm
+											}
+											isInvalid={
+												touched.passwordConfirm && errors.passwordConfirm
+											}
+										/>
+										<Form.Control.Feedback type="invalid">
+											{errors.passwordConfirm}
+										</Form.Control.Feedback>
+									</Form.Group>
+									<Button
+										disabled={isSubmitting}
+										variant="primary"
+										type="submit"
+									>
+										Sign up
+									</Button>
+								</Form>
+							)}
+						</Formik>
+						<Card.Text />
+						{error && (
+							<Alert variant="danger">
+								<Alert.Heading as="h6">{error}</Alert.Heading>
+								<hr />
+								<p className="mb-0">
+									If you have forgotten your password, you can reset your
+									password <Link to="/forgot-password-organization">here</Link>
+								</p>
+							</Alert>
+						)}
+						{message && (
+							<Alert variant="success">
+								<Alert.Heading as="h6">{message}</Alert.Heading>
+								<hr />
+								<p className="mb-0">
+									Please check your inbox for a verification email
+								</p>
+							</Alert>
+						)}
+						<Card.Text />
+						<Card.Footer>
+							Already have an organization account?{" "}
+							<Link to="/sign-in-organization">Sign in here!</Link>
+						</Card.Footer>
+					</Card.Body>
+				</Card>
+			</div>
+		</div>
+	);
 };
 
 export default SignUpOrgForm;
+
+const validationSchema = Yup.object().shape({
+	email: Yup.string()
+		.email("Please enter a valid email address")
+		.required("Please enter your email address"),
+	password: Yup.string()
+		.required("Please enter your password")
+		.matches(
+			/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+			"Your password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special case character"
+		),
+	passwordConfirm: Yup.string()
+		.required("Please confirm you password")
+		.when("password", {
+			is: (password) => password && password.length > 0,
+			then: Yup.string().oneOf(
+				[Yup.ref("password")],
+				"Your passwords do not match"
+			),
+		}),
+});
