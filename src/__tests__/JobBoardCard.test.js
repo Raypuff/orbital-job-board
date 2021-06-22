2687e9ab7a91ac965d40a4ab693e569616e84893
Rayner <raynerljm@gmail.com>
2021-06-22 14:28:57 +0800

    Add fallback image for JobDetails as well and also add some tests

--- src/__tests__/JobBoardCard.test.js
+++ src/__tests__/JobBoardCard.test.js
@@ -1,3 +1,30 @@
 import React from "react";
+import { StaticRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { toBeInTheDocument } from "@testing-library/jest-dom";
 import JobBoardCard from "../components/JOBS/JobBoardCard";
+
+test("thumbnail shows up properly", async () => {
+	const job = render(
+		<StaticRouter>
+			<JobBoardCard imageUrl="/test.png" />
+		</StaticRouter>
+	);
+	const jobThumbnail = await job.findByTestId("thumbnail");
+	expect(jobThumbnail.src).toContain("test.png");
+});
+
+test("clicking learn more leads to job details page", async () => {
+	const job = render(
+		<StaticRouter>
+			<JobBoardCard id="testURL" />
+		</StaticRouter>
+	);
+	expect(screen.getByText("Learn more")).toBeInTheDocument();
+	const leftClick = { button: 0 };
+	expect(screen.getByText("Learn more").closest("a")).toHaveAttribute(
+		"href",
+		"/jobs/testURL"
+	);
+});
