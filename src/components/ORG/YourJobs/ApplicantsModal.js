c35c8c3f455bb909115e52fe4ea0fa9ed0ba2573
Rayner <raynerljm@gmail.com>
2021-06-23 18:02:30 +0800

    Add changed useState in ApplicantsModal

--- src/components/ORG/YourJobs/ApplicantsModal.js
+++ src/components/ORG/YourJobs/ApplicantsModal.js
@@ -4,131 +4,133 @@ import ApplicantsModalCard from "./ApplicantsModalCard";
 import styles from "./ApplicantsModal.module.css";
 
 const ApplicantsModal = ({
-  show,
-  onHide,
-  key,
-  id,
-  status,
-  title,
-  beneficiaries,
-  skills,
-  purpose,
-  platform,
-  multiLocation,
-  location,
-  postalCode,
-  type,
-  flexiDate,
-  longStartDate,
-  longEndDate,
-  flexiHours,
-  longHours,
-  adShift,
-  addInfo,
-  imageUrl,
-  pocName,
-  pocNo,
-  pocEmail,
-  dateCreated,
-  datePosted,
-  applicants,
+	show,
+	onHide,
+	key,
+	id,
+	status,
+	title,
+	beneficiaries,
+	skills,
+	purpose,
+	platform,
+	multiLocation,
+	location,
+	postalCode,
+	type,
+	flexiDate,
+	longStartDate,
+	longEndDate,
+	flexiHours,
+	longHours,
+	adShift,
+	addInfo,
+	imageUrl,
+	pocName,
+	pocNo,
+	pocEmail,
+	dateCreated,
+	datePosted,
+	applicants,
 }) => {
-  //create usestate for job apps
-  const [applications, setApplications] = useState([]);
+	//create usestate for job apps
+	const [applications, setApplications] = useState([]);
+	const [changed, setChanged] = useState(false);
 
-  const getApplications = async () => {
-    const response = await fetch(
-      "https://volunteer-ccsgp-backend.herokuapp.com/job_applications/job/" + id
-    );
-    const jsonData = await response.json();
-    setApplications(jsonData);
-  };
+	const getApplications = async () => {
+		const response = await fetch(
+			"https://volunteer-ccsgp-backend.herokuapp.com/job_applications/job/" + id
+		);
+		const jsonData = await response.json();
+		setApplications(jsonData);
+	};
 
-  useEffect(() => {
-    getApplications();
-  }, []);
+	useEffect(() => {
+		getApplications();
+	}, [changed]);
 
-  const [filterField, setFilterField] = useState("All");
-  const onHideAndResetFilter = () => {
-    onHide();
-    setFilterField("All");
-  };
+	const [filterField, setFilterField] = useState("All");
+	const onHideAndResetFilter = () => {
+		onHide();
+		setFilterField("All");
+	};
 
-  const applicationsFiltered = applications.filter((application) => {
-    if (filterField === "All") {
-      return true;
-    } else {
-      return application.status === filterField;
-    }
-  });
+	const applicationsFiltered = applications.filter((application) => {
+		if (filterField === "All") {
+			return true;
+		} else {
+			return application.status === filterField;
+		}
+	});
 
-  return (
-    <Modal
-      show={show}
-      onHide={onHideAndResetFilter}
-      size="lg"
-      centered
-      scrollable
-    >
-      <Modal.Header closeButton>
-        <div className={styles.titleContainer}>
-          <Modal.Title className={styles.titleWrapper}>
-            <div>{title}</div>
-          </Modal.Title>
-          <Form>
-            <div className={styles.filterWrapper}>
-              <Form.Label>Filter by: </Form.Label>
-              <Form.Group style={{ margin: "0" }}>
-                <Form.Control
-                  as="select"
-                  onChange={(event) => {
-                    setFilterField(event.target.value);
-                  }}
-                >
-                  <option>All</option>
-                  <option>Pending</option>
-                  <option>Accepted</option>
-                  <option>Rejected</option>
-                </Form.Control>
-              </Form.Group>
-            </div>
-          </Form>
-        </div>
-      </Modal.Header>
-      <Modal.Body>
-        {applicationsFiltered.length >= 1 ? (
-          applicationsFiltered.map((application) => {
-            return (
-              <ApplicantsModalCard
-                key={application.id}
-                id={application.id}
-                stuID={application.stuID}
-                jobID={application.jobID}
-                status={application.status}
-                stuAddInfo={application.stuAddInfo}
-                dateApplied={application.dateApplied}
-                name={application.stuName}
-                email={application.stuEmail}
-                contactNo={application.stuNo}
-                course={application.stuCourse}
-                yearOfStudy={application.stuYearOfStudy}
-                title={title}
-              />
-            );
-          })
-        ) : filterField === "All" ? (
-          <div className={styles.emptyState}>
-            There are no applicants for this job
-          </div>
-        ) : (
-          <div className={styles.emptyState}>
-            There are no applicants for this job that fulfil the following
-            filter: {filterField}
-          </div>
-        )}
-      </Modal.Body>
-    </Modal>
-  );
+	return (
+		<Modal
+			show={show}
+			onHide={onHideAndResetFilter}
+			size="lg"
+			centered
+			scrollable
+		>
+			<Modal.Header closeButton>
+				<div className={styles.titleContainer}>
+					<Modal.Title className={styles.titleWrapper}>
+						<div>{title}</div>
+					</Modal.Title>
+					<Form>
+						<div className={styles.filterWrapper}>
+							<Form.Label>Filter by: </Form.Label>
+							<Form.Group style={{ margin: "0" }}>
+								<Form.Control
+									as="select"
+									onChange={(event) => {
+										setFilterField(event.target.value);
+									}}
+								>
+									<option>All</option>
+									<option>Pending</option>
+									<option>Accepted</option>
+									<option>Rejected</option>
+								</Form.Control>
+							</Form.Group>
+						</div>
+					</Form>
+				</div>
+			</Modal.Header>
+			<Modal.Body>
+				{applicationsFiltered.length >= 1 ? (
+					applicationsFiltered.map((application) => {
+						return (
+							<ApplicantsModalCard
+								key={application.id}
+								id={application.id}
+								stuID={application.stuID}
+								jobID={application.jobID}
+								status={application.status}
+								stuAddInfo={application.stuAddInfo}
+								dateApplied={application.dateApplied}
+								name={application.stuName}
+								email={application.stuEmail}
+								contactNo={application.stuNo}
+								course={application.stuCourse}
+								yearOfStudy={application.stuYearOfStudy}
+								title={title}
+								setChanged={setChanged}
+							/>
+						);
+					})
+				) : filterField === "All" ? (
+					<div className={styles.emptyState}>
+						There are no applicants for this job
+					</div>
+				) : (
+					<div className={styles.emptyState}>
+						There are no applicants for this job that fulfil the following
+						filter: {filterField}
+					</div>
+				)}
+			</Modal.Body>
+		</Modal>
+	);
 };
 
 export default ApplicantsModal;
--- src/components/ORG/YourJobs/ApplicantsModalCard.js
+++ src/components/ORG/YourJobs/ApplicantsModalCard.js
@@ -1,150 +1,151 @@
 import { useState } from "react";
 import { Card, Button, Col, Row } from "react-bootstrap";
 import {
-  HourglassSplit,
-  XCircleFill,
-  CheckCircleFill,
+	HourglassSplit,
+	XCircleFill,
+	CheckCircleFill,
 } from "react-bootstrap-icons";
 import styles from "./ApplicantsModalCard.module.css";
 
 const ApplicantsModalCard = ({
-  id,
-  stuID,
-  jobID,
-  status,
-  stuAddInfo,
-  dateApplied,
-  name,
-  email,
-  contactNo,
-  course,
-  yearOfStudy,
-  title,
+	id,
+	stuID,
+	jobID,
+	status,
+	stuAddInfo,
+	dateApplied,
+	name,
+	email,
+	contactNo,
+	course,
+	yearOfStudy,
+	title,
+	setChanged,
 }) => {
-  //const [loading, setLoading] = useState(false);
-  // useState for frontend update of status
-  const [frontEndStatus, setFrontEndStatus] = useState(status);
+	//const [loading, setLoading] = useState(false);
+	// useState for frontend update of status
+	const [frontEndStatus, setFrontEndStatus] = useState(status);
 
-  const handleAcceptReject = async (choice) => {
-    //setLoading(true);
-    const body = { status: choice };
-    try {
-      const updateAppStatus = await fetch(
-        "https://volunteer-ccsgp-backend.herokuapp.com/job_applications/status/" +
-          id,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-        }
-      );
-      setFrontEndStatus(choice);
+	const handleAcceptReject = async (choice) => {
+		//setLoading(true);
+		const body = { status: choice };
+		try {
+			const updateAppStatus = await fetch(
+				"https://volunteer-ccsgp-backend.herokuapp.com/job_applications/status/" +
+					id,
+				{
+					method: "PUT",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(body),
+				}
+			);
+			// setFrontEndStatus(choice);
+			setChanged(true);
 
-      const text = `Hello ${name}! There has been an update to your volunteer application. Please click on the link below and log in to view the updates to your application! volunteer-ccsgp-vercel.app`;
-      const html = `Hello ${name}!<br>There has been an update to your volunteer application. <br>Please click on the link below and log in to view the updates to your application! <a href="volunteer-ccsgp-vercel.app">volunteer-ccsgp-vercel.app</a>`;
-      const msg = {
-        msg: {
-          to: email,
-          from: "volunteerccsgp@gmail.com",
-          subject: `[Volunteer CCSGP] Change in status of your job application for ${title}`,
-          text: text,
-          html: html,
-        },
-      };
-      const sendEmail = await fetch(
-        "https://volunteer-ccsgp-backend.herokuapp.com/email",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(msg),
-        }
-      );
-      console.log(sendEmail);
-    } catch (err) {
-      console.error(err);
-    }
-    //setLoading(false);
-  };
+			const text = `Hello ${name}! There has been an update to your volunteer application. Please click on the link below and log in to view the updates to your application! volunteer-ccsgp-vercel.app`;
+			const html = `Hello ${name}!<br>There has been an update to your volunteer application. <br>Please click on the link below and log in to view the updates to your application! <a href="volunteer-ccsgp-vercel.app">volunteer-ccsgp-vercel.app</a>`;
+			const msg = {
+				msg: {
+					to: email,
+					from: "volunteerccsgp@gmail.com",
+					subject: `[Volunteer CCSGP] Change in status of your job application for ${title}`,
+					text: text,
+					html: html,
+				},
+			};
+			const sendEmail = await fetch(
+				"https://volunteer-ccsgp-backend.herokuapp.com/email",
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify(msg),
+				}
+			);
+			// console.log(sendEmail);
+		} catch (err) {
+			// console.error(err);
+		}
+	};
 
-  return (
-    <Card>
-      <div className={styles.cardContainer}>
-        <Row>
-          <Col lg={9}>
-            <div className={styles.topRowContainer}>
-              <h5>{name}</h5>
-              <div
-                className={
-                  frontEndStatus === "Pending"
-                    ? styles.pending
-                    : styles.displayNone
-                }
-              >
-                <HourglassSplit />
-                Pending
-              </div>
-              <div
-                className={
-                  frontEndStatus === "Rejected"
-                    ? styles.rejected
-                    : styles.displayNone
-                }
-              >
-                <XCircleFill />
-                Rejected
-              </div>
-              <div
-                className={
-                  frontEndStatus === "Accepted"
-                    ? styles.accepted
-                    : styles.displayNone
-                }
-              >
-                <CheckCircleFill />
-                Accepted
-              </div>
-            </div>
-            <h6>Submitted on {dateApplied}</h6>
-            <h6>Course of study: {course}</h6>
-            <h6>Year of study: {yearOfStudy}</h6>
-            <h6>Mobile number: {contactNo}</h6>
-            <h6>Email address: {email}</h6>
-            <h6>
-              Additional information:
-              <br />
-              {stuAddInfo}
-            </h6>
-          </Col>
-          <Col lg={3}>
-            <div className={styles.buttonContainer}>
-              <div className={styles.buttonWrapper}>
-                <div
-                  className={
-                    status === "Pending" ? styles.display : styles.displayNone
-                  }
-                >
-                  <Button
-                    variant="outline-danger"
-                    onClick={(event) => handleAcceptReject(event.target.value)}
-                    value="Rejected"
-                  >
-                    Reject
-                  </Button>
-                  <Button
-                    variant="outline-success"
-                    onClick={(event) => handleAcceptReject(event.target.value)}
-                    value="Accepted"
-                  >
-                    Accept
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    </Card>
-  );
+	return (
+		<Card>
+			<div className={styles.cardContainer}>
+				<Row>
+					<Col lg={9}>
+						<div className={styles.topRowContainer}>
+							<h5>{name}</h5>
+							<div
+								className={
+									frontEndStatus === "Pending"
+										? styles.pending
+										: styles.displayNone
+								}
+							>
+								<HourglassSplit />
+								Pending
+							</div>
+							<div
+								className={
+									frontEndStatus === "Rejected"
+										? styles.rejected
+										: styles.displayNone
+								}
+							>
+								<XCircleFill />
+								Rejected
+							</div>
+							<div
+								className={
+									frontEndStatus === "Accepted"
+										? styles.accepted
+										: styles.displayNone
+								}
+							>
+								<CheckCircleFill />
+								Accepted
+							</div>
+						</div>
+						<h6>Submitted on {dateApplied}</h6>
+						<h6>Course of study: {course}</h6>
+						<h6>Year of study: {yearOfStudy}</h6>
+						<h6>Mobile number: {contactNo}</h6>
+						<h6>Email address: {email}</h6>
+						<h6>
+							Additional information:
+							<br />
+							{stuAddInfo}
+						</h6>
+					</Col>
+					<Col lg={3}>
+						<div className={styles.buttonContainer}>
+							<div className={styles.buttonWrapper}>
+								<div
+									className={
+										status === "Pending" ? styles.display : styles.displayNone
+									}
+								>
+									<Button
+										variant="outline-danger"
+										onClick={(event) => handleAcceptReject(event.target.value)}
+										value="Rejected"
+									>
+										Reject
+									</Button>
+									<Button
+										variant="outline-success"
+										onClick={(event) => handleAcceptReject(event.target.value)}
+										value="Accepted"
+									>
+										Accept
+									</Button>
+								</div>
+							</div>
+						</div>
+					</Col>
+				</Row>
+			</div>
+		</Card>
+	);
 };
 
 export default ApplicantsModalCard;
