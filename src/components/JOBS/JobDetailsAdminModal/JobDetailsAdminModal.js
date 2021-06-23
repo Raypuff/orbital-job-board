e000ed2bf9da627c959dfdaf11f1e4168e0ea1b3
Zechary <zecharyajw@u.nus.edu>
2021-06-23 17:34:42 +0800

    Add Approve/Reject functionality for admins

--- src/components/JOBS/JobDetailsAdminModal/JobDetailsAdminModal.js
+++ src/components/JOBS/JobDetailsAdminModal/JobDetailsAdminModal.js
@@ -1,112 +1,138 @@
 import { Modal, Button } from "react-bootstrap";
 import styles from "./JobDetailsAdminModal.module.css";
 
+const handleAcceptReject = async (jobId, choice) => {
+  const body = { status: choice };
+  try {
+    await fetch(
+      "https://volunteer-ccsgp-backend.herokuapp.com/jobs/status/" + jobId,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export const JobDetailsAdminRejModal = ({
-	show,
-	onHide,
-	id,
-	orgType,
-	orgName,
-	orgEmail,
-	status,
-	title,
-	beneficiaries,
-	skills,
-	purpose,
-	platform,
-	multiLocation,
-	location,
-	postalCode,
-	type,
-	flexiDate,
-	longStartDate,
-	longEndDate,
-	flexiHours,
-	longHours,
-	adShift,
-	addInfo,
-	imageUrl,
-	pocName,
-	pocNo,
-	pocEmail,
-	applicants,
+  show,
+  onHide,
+  id,
+  orgType,
+  orgName,
+  orgEmail,
+  status,
+  title,
+  beneficiaries,
+  skills,
+  purpose,
+  platform,
+  multiLocation,
+  location,
+  postalCode,
+  type,
+  flexiDate,
+  longStartDate,
+  longEndDate,
+  flexiHours,
+  longHours,
+  adShift,
+  addInfo,
+  imageUrl,
+  pocName,
+  pocNo,
+  pocEmail,
+  applicants,
 }) => {
-	return (
-		<Modal show={show} onHide={onHide} centered>
-			<Modal.Header closeButton>
-				<Modal.Title>You are rejecting {title}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<div className={styles.modalContainer}>
-					Are you sure you want to reject this posting?
-					<Button variant="danger">Reject posting</Button>
-				</div>
-			</Modal.Body>
-		</Modal>
-	);
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>You are rejecting {title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className={styles.modalContainer}>
+          Are you sure you want to reject this posting?
+          <Button
+            onClick={(event) => handleAcceptReject(id, "Rejected")}
+            variant="danger"
+          >
+            Reject posting
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export const JobDetailsAdminAppModal = ({
-	show,
-	onHide,
-	id,
-	orgType,
-	orgName,
-	orgEmail,
-	status,
-	title,
-	beneficiaries,
-	skills,
-	purpose,
-	platform,
-	multiLocation,
-	location,
-	postalCode,
-	type,
-	flexiDate,
-	longStartDate,
-	longEndDate,
-	flexiHours,
-	longHours,
-	adShift,
-	addInfo,
-	imageUrl,
-	pocName,
-	pocNo,
-	pocEmail,
-	applicants,
+  show,
+  onHide,
+  id,
+  orgType,
+  orgName,
+  orgEmail,
+  status,
+  title,
+  beneficiaries,
+  skills,
+  purpose,
+  platform,
+  multiLocation,
+  location,
+  postalCode,
+  type,
+  flexiDate,
+  longStartDate,
+  longEndDate,
+  flexiHours,
+  longHours,
+  adShift,
+  addInfo,
+  imageUrl,
+  pocName,
+  pocNo,
+  pocEmail,
+  applicants,
 }) => {
-	return (
-		<Modal show={show} onHide={onHide} centered>
-			<Modal.Header closeButton>
-				<Modal.Title>You are approving {title}</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<div className={styles.modalContainer}>
-					Are you sure you want to approve this posting?
-					<Button variant="success">Approve posting</Button>
-				</div>
-			</Modal.Body>
-		</Modal>
-	);
+  return (
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>You are approving {title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className={styles.modalContainer}>
+          Are you sure you want to approve this posting?
+          <Button
+            onClick={(event) => handleAcceptReject(id, "Approved")}
+            variant="success"
+          >
+            Approve posting
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
-export const AdminOpenRejModalButton = ({ handleClick }) => {
-	return (
-		<>
-			<Button variant="danger" onClick={handleClick}>
-				Reject posting
-			</Button>
-		</>
-	);
+export const AdminRejButton = ({ handleClick }) => {
+  return (
+    <>
+      <Button variant="danger" onClick={handleClick}>
+        Reject posting
+      </Button>
+    </>
+  );
 };
 
-export const AdminOpenAppModalButton = ({ handleClick }) => {
-	return (
-		<>
-			<Button variant="success" onClick={handleClick}>
-				Approve posting
-			</Button>
-		</>
-	);
+export const AdminAppButton = ({ handleClick }) => {
+  return (
+    <>
+      <Button variant="success" onClick={handleClick}>
+        Approve posting
+      </Button>
+    </>
+  );
 };
