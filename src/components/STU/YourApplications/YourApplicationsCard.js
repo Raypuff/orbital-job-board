1bb2d093d611f86ac613c6ff3f796d6bbaf1c883
Zechary <zecharyajw@u.nus.edu>
2021-06-25 18:34:40 +0800

    Add backend functionality for YourApplicationsCard

--- src/components/STU/YourApplications/YourApplicationsCard.js
+++ src/components/STU/YourApplications/YourApplicationsCard.js
@@ -1,144 +1,154 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { Row, Col, Card, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import {
-	ThreeDotsVertical,
-	HourglassSplit,
-	XCircleFill,
-	CheckCircleFill,
+  ThreeDotsVertical,
+  HourglassSplit,
+  XCircleFill,
+  CheckCircleFill,
 } from "react-bootstrap-icons";
 import styles from "./YourApplicationsCard.module.css";
 
 const YourApplicationsCard = ({
-	key,
-	id,
-	stuID,
-	jobID,
-	status,
-	stuAddInfo,
-	dateApplied,
+  key,
+  id,
+  stuID,
+  jobID,
+  status,
+  stuAddInfo,
+  dateApplied,
 }) => {
-	const job = {
-		title: "pp",
-		status: "Approved",
-		platform: "Physical",
-		multiLocation: false,
-		location: "Bukit Panjang",
-		postalCode: "123456",
-		type: "Ad hoc",
-	}; //get jobs where job.id === application.job_id @zech
+  const [job, setJob] = useState(null);
+  const [jobLoading, setJobLoading] = useState(true);
 
-	return (
-		<>
-			<div className={styles.container}>
-				<Card>
-					<Row>
-						<Col lg={8}>
-							<div className={styles.contentContainer}>
-								<div className={styles.topRowContainer}>
-									<div className={styles.titleContainer}>
-										<h4 className={styles.title}>{job.title}</h4>
-										<div
-											className={
-												status === "Pending"
-													? styles.pending
-													: styles.displayNone
-											}
-										>
-											<HourglassSplit className={styles.icons} />
-											<h6>Status: Pending acceptance from organization</h6>
-										</div>
-										<div
-											className={
-												status === "Rejected"
-													? styles.rejected
-													: styles.displayNone
-											}
-										>
-											<XCircleFill className={styles.icons} />
-											<h6>Status: Rejected by organization</h6>
-										</div>
-										<div
-											className={
-												status === "Accepted"
-													? styles.accepted
-													: styles.displayNone
-											}
-										>
-											<CheckCircleFill className={styles.icons} />
-											<h6>Status: Accepted</h6>
-										</div>
-									</div>
-									<div className={styles.dotsContainerMobile}>
-										<TripleDot jobID={jobID} />
-									</div>
-								</div>
+  const getJob = async () => {
+    const response = await fetch(
+      "https://volunteer-ccsgp-backend.herokuapp.com/jobs/" + jobID
+    );
+    const jsonData = await response.json();
+    setJob(jsonData);
+    setJobLoading(false);
+  };
 
-								<h6>Applied on: {new Date(dateApplied).toDateString()}</h6>
-								<h6>
-									Job status:{" "}
-									{job.status !== "Completed" ? "In progress" : "Completed"}
-								</h6>
+  useEffect(() => {
+    getJob();
+  }, [job]);
 
-								<h6>
-									Location:{" "}
-									{job.platform === "Virtual"
-										? "Virtual"
-										: job.platform === "Physical" && job.multiLocation
-										? "Multiple locations"
-										: job.platform === "Physical" && !job.multiLocation
-										? `${job.location} S(${job.postalCode})`
-										: ""}
-								</h6>
-								<h6>Commitment type: {job.type}</h6>
-								<h6>
-									Your additional information:
-									<br />
-									{stuAddInfo}
-								</h6>
-							</div>
-						</Col>
-						<Col lg={4}>
-							<div className={styles.applicantsContainer}>
-								<div className={styles.dotsContainer}>
-									<TripleDot jobID={jobID} />
-								</div>
-							</div>
-						</Col>
-					</Row>
-				</Card>
-			</div>
-		</>
-	);
+  if (jobLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Card>
+          <Row>
+            <Col lg={8}>
+              <div className={styles.contentContainer}>
+                <div className={styles.topRowContainer}>
+                  <div className={styles.titleContainer}>
+                    <h4 className={styles.title}>{job.title}</h4>
+                    <div
+                      className={
+                        status === "Pending"
+                          ? styles.pending
+                          : styles.displayNone
+                      }
+                    >
+                      <HourglassSplit className={styles.icons} />
+                      <h6>Status: Pending acceptance from organization</h6>
+                    </div>
+                    <div
+                      className={
+                        status === "Rejected"
+                          ? styles.rejected
+                          : styles.displayNone
+                      }
+                    >
+                      <XCircleFill className={styles.icons} />
+                      <h6>Status: Rejected by organization</h6>
+                    </div>
+                    <div
+                      className={
+                        status === "Accepted"
+                          ? styles.accepted
+                          : styles.displayNone
+                      }
+                    >
+                      <CheckCircleFill className={styles.icons} />
+                      <h6>Status: Accepted</h6>
+                    </div>
+                  </div>
+                  <div className={styles.dotsContainerMobile}>
+                    <TripleDot jobID={jobID} />
+                  </div>
+                </div>
+
+                <h6>Applied on: {new Date(dateApplied).toDateString()}</h6>
+                <h6>
+                  Job status:{" "}
+                  {job.status !== "Completed" ? "In progress" : "Completed"}
+                </h6>
+
+                <h6>
+                  Location:{" "}
+                  {job.platform === "Virtual"
+                    ? "Virtual"
+                    : job.platform === "Physical" && job.multiLocation
+                    ? "Multiple locations"
+                    : job.platform === "Physical" && !job.multiLocation
+                    ? `${job.location} S(${job.postalCode})`
+                    : ""}
+                </h6>
+                <h6>Commitment type: {job.type}</h6>
+                <h6>
+                  Your additional information:
+                  <br />
+                  {stuAddInfo}
+                </h6>
+              </div>
+            </Col>
+            <Col lg={4}>
+              <div className={styles.applicantsContainer}>
+                <div className={styles.dotsContainer}>
+                  <TripleDot jobID={jobID} />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default YourApplicationsCard;
 
 const CustomDropdown = forwardRef(({ children, onClick }, ref) => (
-	// eslint-disable-next-line jsx-a11y/anchor-is-valid
-	<a
-		href=""
-		ref={ref}
-		onClick={(event) => {
-			event.preventDefault();
-			onClick(event);
-		}}
-	>
-		<ThreeDotsVertical className={styles.dots} />
-	</a>
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
+  <a
+    href=""
+    ref={ref}
+    onClick={(event) => {
+      event.preventDefault();
+      onClick(event);
+    }}
+  >
+    <ThreeDotsVertical className={styles.dots} />
+  </a>
 ));
 
 const TripleDot = ({ jobID }) => {
-	return (
-		<Dropdown>
-			<Dropdown.Toggle as={CustomDropdown}></Dropdown.Toggle>
-			<Dropdown.Menu align="right">
-				<Dropdown.Item as={Link} to={`/jobs/${jobID}`} target="blank">
-					View listing
-				</Dropdown.Item>
-			</Dropdown.Menu>
-		</Dropdown>
-	);
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as={CustomDropdown}></Dropdown.Toggle>
+      <Dropdown.Menu align="right">
+        <Dropdown.Item as={Link} to={`/jobs/${jobID}`} target="blank">
+          View listing
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 };
 
 // function tConvert(time) {
