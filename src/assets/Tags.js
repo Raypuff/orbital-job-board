a61cdfeafdcf465a9d74ed179b6e7e65e74ced61
Rayner <raynerljm@gmail.com>
2021-06-30 14:10:31 +0800

    Improve beneficiary and skill multiselect and add landing page animations

--- src/assets/Tags.js
+++ src/assets/Tags.js
@@ -24,3 +24,31 @@ export const SkillTags = [
 	"WebDev",
 	"Others",
 ];
+
+export const SelectBeneficiaryTags = [
+	{ value: "Animals", label: "Animals" },
+	{ value: "Children", label: "Children" },
+	{ value: "Disability", label: "Disability" },
+	{ value: "Education", label: "Education" },
+	{ value: "Elderly", label: "Elderly" },
+	{ value: "Environment", label: "Environment" },
+	{ value: "Health", label: "Health" },
+	{ value: "Heritage", label: "Heritage" },
+	{ value: "Sports", label: "Sports" },
+	{ value: "Women", label: "Women" },
+	{ value: "Youths", label: "Youths" },
+	{ value: "Other", label: "Other" },
+];
+
+export const SelectSkillTags = [
+	{ value: "Database", label: "Database" },
+	{ value: "Java", label: "Java" },
+	{ value: "Javascript", label: "Javascript" },
+	{ value: "Marketing", label: "Marketing" },
+	{ value: "MobileDev", label: "MobileDev" },
+	{ value: "Planning", label: "Planning" },
+	{ value: "Python", label: "Python" },
+	{ value: "Software", label: "Software" },
+	{ value: "WebDev", label: "WebDev" },
+	{ value: "Others", label: "Others" },
+];
