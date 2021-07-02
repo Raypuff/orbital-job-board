1ea75f790a9ff0ac5e66350a409e02c76ad19b0b
Rayner <raynerljm@gmail.com>
2021-07-02 12:14:51 +0800

    Built Chat UI with react-chat-elements library

--- src/components/STU/ChatStu/ChatStu.js
+++ src/components/STU/ChatStu/ChatStu.js
@@ -4,211 +4,300 @@ import ChatStuMessage from "./ChatStuMessage";
 import { Row, Col, Card, Form, Button } from "react-bootstrap";
 import { Telegram } from "react-bootstrap-icons";
 import { useAuth } from "../../../contexts/AuthContext";
-import { ChatItem, MessageList } from "react-chat-elements";
+import {
+	ChatList,
+	MessageList,
+	Input as ChatInput,
+	Button as ChatButton,
+} from "react-chat-elements";
+import {
+	LoadingChats,
+	NoChats,
+	LoadingMessages,
+	SelectMessage,
+	NoMessage,
+} from "./EmptyStates";
 import styles from "./ChatStu.module.css";
 import image from "../../../assets/noImage.png";
 var uniqid = require("uniqid");
 
 const ChatStu = () => {
-  const { currentUser } = useAuth();
-  const [currentChat, setCurrentChat] = useState(""); //currentChat is the ID of the current open chat
-  const [chats, setChats] = useState(); //chats store all MY chats
-  const [currentMessages, setCurrentMessages] = useState(); //currentMessages store all messages of current chat
-  const [loadingChats, setLoadingChats] = useState(true);
-  const [loadingMessages, setLoadingMessages] = useState(false);
-  const newMessageRef = useRef();
+	const { currentUser } = useAuth();
+	const [currentChat, setCurrentChat] = useState(""); //currentChat is the ID of the current open chat
+	const [chats, setChats] = useState(); //chats store all MY chats
+	const [currentMessages, setCurrentMessages] = useState(); //currentMessages store all messages of current chat
+	const [loadingChats, setLoadingChats] = useState(true);
+	const [loadingMessages, setLoadingMessages] = useState(false);
+	const newMessageRef = useRef();
 
-  //fetch chats where chat.stuID === currentUser.email
-  const getChats = async () => {
-    const chatData = await fetch(
-      process.env.REACT_APP_BACKEND_URL +
-        "/chats/all-chats/" +
-        currentUser.email
-    );
-    const chats = await chatData.json();
-    console.log(`Printing Chats:`);
-    console.log(chats);
-    setChats(chats);
-    setLoadingChats(false);
-  };
+	//fetch chats where chat.stuID === currentUser.email
+	const getChats = async () => {
+		const chatData = await fetch(
+			process.env.REACT_APP_BACKEND_URL +
+				"/chats/all-chats/" +
+				currentUser.email
+		);
+		const chats = await chatData.json();
+		console.log(`Printing Chats:`);
+		console.log(chats);
+		// setChats(chats);
+		setChats(dummyChats);
+		setLoadingChats(false);
+	};
 
-  useEffect(() => {
-    getChats();
-  }, [currentChat]);
+	useEffect(() => {
+		getChats();
+	}, [currentChat]);
 
-  //fetch messages where message.id === currentChat (set loadingMessages true then false), call everytime currentChat changes, if message.status === "Sent", update to "Read"
-  const getMessages = async () => {
-    const messagesData = await fetch(
-      process.env.REACT_APP_BACKEND_URL + "/chats/messages/" + currentChat
-    );
-    const messages = await messagesData.json();
-    setCurrentMessages(messages);
-    setLoadingMessages(false);
-  };
+	//fetch messages where message.id === currentChat (set loadingMessages true then false), call everytime currentChat changes, if message.status === "Sent", update to "Read"
+	const getMessages = async () => {
+		const messagesData = await fetch(
+			process.env.REACT_APP_BACKEND_URL + "/chats/messages/" + currentChat
+		);
+		const messages = await messagesData.json();
+		// setCurrentMessages(messages);
+		setCurrentMessages(dummyMessages);
+		setLoadingMessages(false);
+	};
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const newMessageID = uniqid();
-    const newMessage = {
-      id: newMessageID,
-      chatID: currentChat,
-      fromID: currentUser.email,
-      text: newMessageRef.current.value,
-      date: new Date().toGMTString(),
-    };
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const newMessageID = uniqid();
+		const frontendMessage = {
+			id: newMessageID,
+			chatID: currentChat,
+			position: "right",
+			fromID: currentUser.email,
+			type: "text",
+			text: newMessageRef.current.value,
+			date: new Date(),
+		};
+		const backendMessage = {
+			id: newMessageID,
+			chatID: currentChat,
+			fromID: currentUser.email,
+			type: "text",
+			text: newMessageRef.current.value,
+			date: new Date().toUTCString(),
+		};
 
-    //push shit here
-    setCurrentMessages(currentMessages.concat([newMessage]));
-    //push new message into messages
-    //update the chat where chat.id === currentChat to have chat.lastDateTime = newMessage.dateTime and chat.lastContent = newMessage.message
+		setCurrentMessages(currentMessages.concat([frontendMessage]));
+		//update the chat where chat.id === currentChat to have chat.lastDateTime = newMessage.dateTime and chat.lastContent = newMessage.message and unread +=1
 
-    try {
-      await fetch(process.env.REACT_APP_BACKEND_URL + "/chats/" + currentChat, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(newMessage),
-      });
-    } catch (err) {
-      console.error(err);
-    }
+		try {
+			await fetch(process.env.REACT_APP_BACKEND_URL + "/chats/" + currentChat, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(backendMessage),
+			});
+		} catch (err) {
+			console.error(err);
+		}
 
-    newMessageRef.current.value = "";
-  };
+		newMessageRef.current.value = "";
+	};
 
-  /*
-  if (loadingChats || loadingMessages) {
-    return <div>im loading!</div>;
-  }
-  */
+	if (loadingChats) {
+		return <LoadingChats />;
+	}
 
-  return (
-    <div className={styles.container}>
-      {chats && chats.length > 0 ? (
-        <Row>
-          <Col lg={4} className={styles.chatCol}>
-            <Card className={styles.chatContainer}>
-              {chats.map((chat) => (
-                <ChatItem
-                  avatar={image}
-                  alt="placeholder"
-                  title={chat.orgID}
-                  subtitle={chat.subtitle}
-                  date={new Date(chat.date)}
-                  unread={chat.unread}
-                  onClick={() => {
-                    setCurrentChat(chat.id);
-                    setLoadingMessages(true);
-                    getMessages();
-                  }}
-                />
-              ))}
-            </Card>
-          </Col>
-          <Col lg={8} className={styles.messageCol}>
-            <Card
-              className={
-                currentChat
-                  ? styles.messageContainer
-                  : styles.noMessageContainer
-              }
-            >
-              {!currentChat ? (
-                <div>select a msg lol</div>
-              ) : currentChat && loadingMessages ? (
-                <div>loading messages lol</div>
-              ) : currentChat && currentMessages ? (
-                currentMessages.length === 0 ? (
-                  <div>send a msg or smth!!</div>
-                ) : (
-                  currentMessages
-                    .filter((msg) => msg.chatID === currentChat)
-                    .sort((msg1, msg2) => {
-                      return new Date(msg1.date) - new Date(msg2.date);
-                    })
-                    .map((msg) => (
-                      <ChatStuMessage
-                        key={msg.id}
-                        id={msg.id}
-                        chatID={msg.chatID}
-                        fromID={msg.fromID}
-                        message={msg.text}
-                        date={msg.date}
-                      />
-                    ))
-                )
-              ) : (
-                <div>this is a state i was not prepared for</div>
-              )}
-            </Card>
-            <Form onSubmit={handleSubmit}>
-              <div
-                className={currentChat ? styles.formRow : styles.displayNone}
-              >
-                <Form.Control ref={newMessageRef} />
-                <Button type="submit">
-                  <Telegram style={{ color: "white" }} />
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-      ) : (
-        <div> u got no chats lol</div>
-      )}
-    </div>
-  );
+	if (chats.length > 0) {
+		return (
+			<div className={styles.container}>
+				{chats && chats.length > 0 ? (
+					<Row>
+						<Col lg={4} className={styles.chatCol}>
+							<Card className={styles.chatContainer}>
+								<ChatList
+									className="chat-list"
+									dataSource={chats.sort(
+										(chat1, chat2) => chat2.date - chat1.date
+									)}
+									onClick={(chat) => {
+										setCurrentChat(chat.id);
+										setLoadingMessages(true);
+										getMessages();
+									}}
+								/>
+							</Card>
+						</Col>
+						<Col lg={8} className={styles.messageCol}>
+							<Card
+								className={
+									currentChat
+										? styles.messageContainer
+										: styles.noMessageContainer
+								}
+							>
+								{!currentChat ? (
+									<SelectMessage />
+								) : currentChat && loadingMessages ? (
+									<LoadingMessages />
+								) : currentChat && currentMessages ? (
+									currentMessages.length === 0 ? (
+										<NoMessage />
+									) : (
+										<MessageList
+											className="message-list"
+											lockable={true}
+											toBottomHeight={"100%"}
+											dataSource={currentMessages
+												.filter((msg) => msg.chatID === currentChat)
+												.sort((msg1, msg2) => msg1.date - msg2.date)}
+										/>
+									)
+								) : (
+									<div>this is a state i was not prepared for</div>
+								)}
+							</Card>
+							<Form onSubmit={handleSubmit}>
+								<div
+									className={currentChat ? styles.formRow : styles.displayNone}
+								>
+									<Form.Control
+										placeholder="Write a message..."
+										ref={newMessageRef}
+									/>
+									<Button type="submit">
+										<Telegram style={{ color: "white" }} />
+									</Button>
+								</div>
+							</Form>
+						</Col>
+					</Row>
+				) : (
+					<div> u got no chats lol</div>
+				)}
+			</div>
+		);
+	} else {
+		return <NoChats />;
+	}
 };
 
 export default ChatStu;
 
 const databaseChat = {
-  id: "1",
-  stuAvatar: "url",
-  orgAvatar: "url",
-  alt: "avatar",
-  stuID: "",
-  orgID: "",
-  //stuTitle: "", //fetch student_account.name where student_account.ID === stuID
-  //orgTitle: "", //fetch organization_account.name where organization_account.ID === orgID
-  subtitle: "lastMessage", //if fromID === currentUser.email ? "You:" : ""
-  fromID: "", //who the last message is from
-  date: "lastDate",
-  unread: "", //+=1 everytime a new message is sent
+	id: "1",
+	stuAvatar: "url",
+	orgAvatar: "url",
+	alt: "avatar",
+	stuID: "",
+	orgID: "",
+	//stuTitle: "", //fetch student_account.name where student_account.ID === stuID
+	//orgTitle: "", //fetch organization_account.name where organization_account.ID === orgID
+	subtitle: "lastMessage", //if fromID === currentUser.email ? "You:" : ""
+	fromID: "", //who the last message is from
+	date: "lastDate",
+	unread: "", //+=1 everytime a new message is sent
 };
 
 const frontendChat = {
-  id: "1",
-  avatar: "url",
-  alt: "avatar",
-  stuID: "",
-  orgID: "",
-  title: "",
-  subtitle: "lastMessage", //if fromID === currentUser.email ? "You:" : ""
-  fromID: "", //who the last message is from
-  date: "lastDate",
-  unread: "", //+=1 everytime a new message is sent
+	id: "1",
+	avatar: "url",
+	alt: "avatar",
+	stuID: "",
+	orgID: "",
+	title: "",
+	subtitle: "lastMessage", //if fromID === currentUser.email ? "You:" : ""
+	fromID: "", //who the last message is from
+	date: "lastDate",
+	unread: "", //+=1 everytime a new message is sent
 };
 
 const databaseMessage = {
-  id: "1",
-  chatID: "1",
-  //position: "", //fromID === currentUser.email ? "right" : "left"
-  fromID: "", //sender's ID
-  type: "text",
-  text: "message",
-  date: "", //new Date().toGMTString() (tentatively)
+	id: "1",
+	chatID: "1",
+	//position: "", //fromID === currentUser.email ? "right" : "left"
+	fromID: "", //sender's ID
+	type: "text",
+	text: "message",
+	date: "", //new Date().toGMTString() (tentatively)
 };
 
 const frontendMessage = {
-  id: "1",
-  chatID: "1",
-  position: "", //fromID === currentUser.email ? "right" : "left"
-  fromID: "", //sender's ID
-  type: "text",
-  text: "message",
-  date: "", //new Date().toGMTString() (tentatively)
+	id: "1",
+	chatID: "1",
+	position: "", //fromID === currentUser.email ? "right" : "left"
+	fromID: "", //sender's ID
+	type: "text",
+	text: "message",
+	date: "", //new Date().toGMTString() (tentatively)
 };
 
-const dummyChats = [];
+const dummyChats = [
+	{
+		id: "bcd",
+		avatar:
+			"https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+		alt: "chat avatar",
+		stuID: "raynerljm@u.nus.edu",
+		orgID: "zecharyajw@gmail.com",
+		title: "Zechary's Charities",
+		subtitle: "You: What is the minimum required hours for this job?",
+		fromID: "zechary@gmail.com",
+		date: new Date("Fri, 01 Jul 2021 21:00:00 GMT"),
+		unread: "1",
+	},
+	{
+		id: "abc",
+		avatar:
+			"https://toppng.com/uploads/preview/roger-berry-avatar-placeholder-11562991561rbrfzlng6h.png",
+		alt: "chat avatar",
+		stuID: "raynerljm@u.nus.edu",
+		orgID: "raynerljm@gmail.com",
+		title: "Saturday Kids",
+		subtitle: "So what's your question for me?",
+		fromID: "raynerljm@gmail.com",
+		date: new Date("Fri, 02 Jul 2021 03:00:00 GMT"),
+		unread: "2",
+	},
+];
 
-const dummyMessages = [];
+const dummyMessages = [
+	{
+		id: "1",
+		chatID: "abc",
+		position: "right",
+		fromID: "raynerljm@u.nus.edu",
+		type: "text",
+		text: "Hi Saturday Kids, can I ask a question?",
+		date: new Date("Fri, 02 Jul 2021 02:00:00 GMT"),
+	},
+	{
+		id: "2",
+		chatID: "abc",
+		position: "left",
+		fromID: "raynerljm@gmail.com",
+		type: "text",
+		text: "Of course you can!",
+		date: new Date("Fri, 02 Jul 2021 02:02:00 GMT"),
+	},
+	{
+		id: "3",
+		chatID: "abc",
+		position: "left",
+		fromID: "raynerljm@gmail.com",
+		type: "text",
+		text: "So what's your question for me?",
+		date: new Date("Fri, 02 Jul 2021 02:02:20 GMT"),
+	},
+	{
+		id: "4",
+		chatID: "bcd",
+		position: "right",
+		fromID: "raynerljm@u.nus.edu",
+		type: "text",
+		text: "Hey Zechary - I have a question...",
+		date: new Date("Fri, 01 Jul 2021 20:59:30 GMT"),
+	},
+	{
+		id: "5",
+		chatID: "bcd",
+		position: "right",
+		fromID: "raynerljm@u.nus.edu",
+		type: "text",
+		text: "What is the minimum required hours for this job?",
+		date: new Date("Fri, 01 Jul 2021 21:00:00 GMT"),
+	},
+];
