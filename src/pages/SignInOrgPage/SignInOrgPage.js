1d1ad69b71e29bc5ff6221abd7c325be619aa749
Rayner <raynerljm@gmail.com>
2021-07-04 12:49:14 +0800

    Restrictions for sign in and sign up so only signed out users can access those pages

--- src/pages/SignInOrgPage/SignInOrgPage.js
+++ src/pages/SignInOrgPage/SignInOrgPage.js
@@ -1,11 +1,19 @@
 import SignInOrgForm from "../../components/ORG/SignInOrgForm";
+import { SignedIn } from "./EmptyStates";
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignInOrgPage = () => {
-	return (
-		<>
-			<SignInOrgForm />
-		</>
-	);
+	const { currentUser } = useAuth();
+
+	function isSignedIn() {
+		if (currentUser) {
+			return <SignedIn />;
+		} else {
+			return <SignInOrgForm />;
+		}
+	}
+
+	return <>{isSignedIn()}</>;
 };
 
 export default SignInOrgPage;
--- src/pages/SignInStuPage/SignInStuPage.js
+++ src/pages/SignInStuPage/SignInStuPage.js
@@ -1,10 +1,18 @@
 import SignInStuForm from "../../components/STU/SignInStuForm";
+import { SignedIn } from "./EmptyStates";
+import { useAuth } from "../../contexts/AuthContext";
 
 const SignInStuPage = () => {
-	return (
-		<>
-			<SignInStuForm />
-		</>
-	);
+	const { currentUser } = useAuth();
+
+	function isSignedIn() {
+		if (currentUser) {
+			return <SignedIn />;
+		} else {
+			return <SignInStuForm />;
+		}
+	}
+
+	return <>{isSignedIn()}</>;
 };
 export default SignInStuPage;
