e7f451297dab5a06bf4839923adc060aaa414c25
Rayner <raynerljm@gmail.com>
2021-07-13 00:57:13 +0800

    Fix bug where non owners can edit jobs

--- src/pages/EditJobsPage/EditJobsPage.js
+++ src/pages/EditJobsPage/EditJobsPage.js
@@ -1,12 +1,20 @@
 import EditJobs from "../../components/ORG/EditJobs";
+import { useAuth } from "../../contexts/AuthContext";
+import { NotOrg } from "./EmptyStates";
 import { withRouter } from "react-router-dom";
 
 const EditJobsPage = ({ match }) => {
-	return (
-		<>
-			<EditJobs id={match.params.id} />
-		</>
-	);
+	const { currentUser, userType } = useAuth();
+
+	function isSignedInOrg() {
+		if (currentUser !== null && userType === "organization") {
+			return <EditJobs id={match.params.id} />;
+		} else {
+			return <NotOrg />;
+		}
+	}
+
+	return <>{isSignedInOrg()} </>;
 };
 
 const EditJobsPageWithRouter = withRouter(EditJobsPage);
