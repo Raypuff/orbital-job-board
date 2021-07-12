63341e5bb1a97f81b8d6dfa031d9069e8510634f
Zechary <zecharyajw@u.nus.edu>
2021-07-13 00:18:17 +0800

    Add editjob functionality

--- src/components/ORG/EditJobs/ConfirmModal.js
+++ src/components/ORG/EditJobs/ConfirmModal.js
@@ -1,12 +1,39 @@
 import { Modal, Button, Form, Alert, Spinner, Row, Col } from "react-bootstrap";
+import { useDist } from "../../../contexts/DistContext";
 
 const ConfirmModal = ({ job, show, onHide }) => {
-  const updateEdits = () => {
-    //set status as pending
-    //update lat and lng according to new postal code (if it is physical and !multiLocation)
-    //remove date posted(?)
+  const { getGeocode } = useDist();
 
-    console.log(job);
+  async function getLatLng(job) {
+    var lat;
+    var lng;
+    var result;
+    if (job.postalCode) {
+      result = await getGeocode(`${job.postalCode}`);
+      lat = result[0];
+      lng = result[1];
+    }
+    job["lat"] = lat;
+    job["lng"] = lng;
+
+    return job;
+  }
+
+  const updateEdits = async () => {
+    try {
+      var editJob = await getLatLng(job);
+      editJob.datePosted = null;
+      editJob.status = "Pending";
+      editJob = { editJob: editJob };
+      await fetch(`${process.env.REACT_APP_BACKEND_URL}/jobs/edit/${job.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(editJob),
+      });
+      window.location.reload(false);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
